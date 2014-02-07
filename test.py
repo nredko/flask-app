e0571b1ecf13c9ea31043f5d8b6dc6395bf52641
nredko <nredko@gmail.com>
2014-02-07 19:00:20 +0700

    c

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 #!env/bin/python
 
 import feedparser
-
+from app import model as m
 rss = feedparser.parse('http://flibusta.net/polka/show/all/rss', '"1391600766"')
 if rss.status == 200:
     print rss.feed.title
     for entry in rss.entries:
         print entry.title
+        post = m.Post(entry.title, entry.summary, entry.published_parsed)
