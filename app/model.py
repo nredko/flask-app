29faf900a1dfd31dc687db4223969b0f7b52ddad
nredko <nredko@gmail.com>
2014-02-26 19:17:50 +0700

    model

--- app/model.py
+++ app/model.py
@@ -2,6 +2,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
@@ -14,13 +15,63 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+class Config(db.model):
+    param = db.Column(db.String(20), primary_key=True)
+    value = db.Column(db.String(120))
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return '<Author %d: %r>' % (self.id, self.name)
+
+
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return '<Genre %d: %r>' % (self.id, self.name)
+
+genres = db.Table('genres',
+                db.Column('genre_id', db.Integer, db.ForeignKey('genre.id')),
+                db.Column('book_id', db.Integer, db.ForeignKey('book.id'))
+)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    author = db.Column(db.Integer, db.ForeignKey('author.id'))
+    genres = db.relationship('Genre', secondary=genres,
+                       backref=db.backref('genres', lazy='dynamic'))
+
+    def __init__(self, id, name, author, genres):
+        self.id = id
+        self.author = author
+        self.genres = genres
+        self.name = name
+
+    def __repr__(self):
+        return '<Author %r>' % self.name
+
 
 class Post(db.Model):
     id = db.Column(db.String(50), primary_key=True)
     title = db.Column(db.String(80))
     body = db.Column(db.Text)
     pub_date = db.Column(db.DateTime)
-
+    user = db.Column(db.String(50))
+    book = db.Column(db.Integer,db.ForeignKey('book.id'))
     # category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     # category = db.relationship('Category',
     #     backref=db.backref('posts', lazy='dynamic'))
--- test.py
+++ test.py
@@ -2,9 +2,11 @@
 
 import feedparser
 from app import model as m
-rss = feedparser.parse('http://flibusta.net/polka/show/all/rss', '"1391600766"')
+
+rss = feedparser.parse('http://flibusta.net/polka/show/all/rss', '" 1393317804"')
 if rss.status == 200:
     print rss.feed.title
+    print rss.etag
     for entry in rss.entries:
         print entry.title
         post = m.Post(entry.title, entry.summary, entry.published_parsed)
