b82aa515db85df0be98ec3ea18c651c2f4ca8367
nredko@home <nredko@gmail.com>
2014-03-26 23:37:13 +0700

    1

--- app/__init__.py
+++ app/__init__.py
@@ -97,6 +97,7 @@ def templated(template=None):
 
 def exec_sql(sql, params):
     result = db.session.execute(sql, params)
+    db.session.commit()
     if not result.return_rows:
         return
     ret = [dict(x) for x in result]
